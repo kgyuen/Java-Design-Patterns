26b1e134acc9362a7340ea263caafebc207a3b83
gyuen <2473929292@qq.com>
2023-02-03 17:49:27 +0800

    单例模式

---
+++ src/main/java/creatation/singleton/MainTest.java
@@ -0,0 +1,21 @@
+package creatation.singleton;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Person person1 = Person.Boss();
+
+        Person person2 = Person.Boss();
+
+        System.out.println(person1 == person2);
+
+        Properties properties = System.getProperties();
+        System.out.println(properties);
+
+        //获取当前系统的环境变量
+        Map<String, String> getenv = System.getenv();
+        System.out.println(getenv);
+    }
+}
